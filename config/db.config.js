3a6dcdca682443648026c1bdb9a81c8c446f7c11
bimoajif <65409867+bimoajif@users.noreply.github.com>
2021-11-22 16:19:06 +0700

    add online database

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url : "mongodb://localhost:27017/paw2"
+    url : "mongodb://bimoajif:Password@cluster0-shard-00-00.vjy66.mongodb.net:27017,cluster0-shard-00-01.vjy66.mongodb.net:27017,cluster0-shard-00-02.vjy66.mongodb.net:27017/paw?ssl=true&replicaSet=atlas-13ew1s-shard-0&authSource=admin&retryWrites=true&w=majority"
 }
